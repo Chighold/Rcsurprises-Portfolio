63cb829f18da75f3aea949b89e6d708582637ca1
Ohagwam Ruth <chidinmaohagwam@gmail.com>
2025-09-01 14:56:51 -0400

    my first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import Logo from './assets/rc.webp'
+
+function Navbar(){
+    return (
+        
+        <div>
+            <div className="navbar"></div>
+            <div className="leftside"></div>
+            <img src={Logo} />
+        </div>
+    )
+}
+export default Navbar
